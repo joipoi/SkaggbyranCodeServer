a4e79ae4c498ffb21fd0eef7deacb2c54777a0a5
joipoi <joel@ekne.com>
2024-11-04 11:45:52 +0100

    tiny change

--- html/index.php
+++ html/index.php
@@ -12,25 +12,25 @@ session_start();
 <body>
     <h1>Upload A Frontend Project</h1>
 
-    <?php if (isset($_SESSION['username'])): ?>
-        <p>Welcome, <?= htmlspecialchars($_SESSION['username']) ?>!</p>
-	<form action="upload.php" method="POST" enctype="multipart/form-data">
-<label for="projectname">Enter Your Project Name:</label>
-        <input type="text" name="projectname" required> <br>
-            <input type="hidden" name="username" value="<?= htmlspecialchars($_SESSION['username']) ?>">
-            <input type="file" name="files[]" multiple required>
-            <input type="submit" value="Upload">
-        </form>
-    <?php else: ?>
-        <p>Either upload as a guest or login first</p>
-	<form action="upload.php" method="POST" enctype="multipart/form-data">
-<label for="projectname">Enter Your Project Name:</label>
-<input type="text" name="projectname" required> <br>
-	    <input type="hidden" name="username" value="guest">
-<input type="file" name="files[]" multiple required>
-            <input type="submit" value="Upload as Guest">
-        </form>
-    <?php endif; ?>
+<?php
+$username = isset($_SESSION['username']) ? htmlspecialchars($_SESSION['username']) : 'guest';
+$welcomeMessage = isset($_SESSION['username']) ? "Welcome $username!" : "Please log in to upload files. You can also upload as a guest.";
+$uploadButtonText = isset($_SESSION['username']) ? "Upload" : "Upload as Guest";
+?>
+
+<p id="welcome"><?= $welcomeMessage ?></p>
+
+<form action="upload.php" method="POST" enctype="multipart/form-data" id="formWrap">
+    <label for="projectname">Name your project then select files:</label><br>
+    <input type="text" name="projectname" placeholder="Project Name" required>
+    
+    <input type="hidden" name="username" value="<?= $username ?>">
+    
+    <label for="files[]" class="fileButton">Select files</label>
+    <input type="file" name="files[]" webkitdirectory multiple required>
+    
+    <button type="submit" id="uploadBtn"><?= $uploadButtonText ?></button>
+</form>
 
     <p><a href="login.php">Login</a> | <a href="register.php">Register</a> | <a href="projects.php">View All Projects</a> | <a href="logout.php">Logout</a> </p>
 </body>
